f908eafd34c59bff105f898b2563185eb58ac8fa
chris31243 <christophermyers@btinternet.com>
2017-09-08 15:02:34 +0100

    Delete dbh.inc.php

--- notehub/includes/dbh.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dbServerName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbNameUsers = "notehubusers";
-$conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbNameUsers);
